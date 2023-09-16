2a85fab70c9ae9d2a6e130f3de14c6c3d09d36d4
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-09-16 11:11:31 +0100

    Update product_recommendation.py

--- AI_Stanford.U/product_recommendation.py
+++ AI_Stanford.U/product_recommendation.py
@@ -3,19 +3,19 @@ import collections
 def extractWordFeatures(x):
   """Extract word features for a review (string) as input and returns a feature vector ϕ(x) for product recommendation
 
-This code works by first checking if the word "great" is in the dictionary. If it is, the value of the positive key is decremented by 1. This is because the word "love" is a positive word, but it is being used in a negative context in the review
+This code works by first checking if the word "great" is in the dictionary. If it is, the value of the positive key is decremented by 1. 
+This is because the word "love" is a positive word, but it is being used in a negative context in the review
+  
   Returns:
     A dict representing the feature vector ϕ(x).
   """
-
   wordDict = collections.defaultdict(float)
   for word in x.split():
     if word in ["good", "great", "excellent", "love", "light", "working", "recommend"]:
       wordDict["positive"] += 1
     elif word in ["bad", "terrible", "awful", "hate", "dark", "broken", "disrecommend"]:
       wordDict["negative"] += 1
-  if "love" in wordDict:
-    wordDict["positive"] -= 1
+
   return wordDict
 
 print(extractWordFeatures("Godtouch apartment stay was great, but AC was broken and food was bad which gave me food poisoning"))
