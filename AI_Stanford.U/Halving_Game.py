05c709a5782f5e0cb98a334f8df2fc74a95821ee
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-10-08 23:54:15 +0100

    BY GDDG

---
+++ AI_Stanford.U/Halving_Game.py
@@ -0,0 +1,55 @@
+class HalvingGame(object):
+    def __init__(self, N):
+        self.N = N
+
+    # state = (god, number)
+
+    def startState(self):
+        return (+1, self.N)
+
+    def isEnd(self, state):
+        god, number = state
+        return number == 0
+
+    def utility(self, state):
+        state = god, number
+        assert number == 0
+        return god * float('inf')
+
+    def actions(self, state):
+        return ['-', '/']
+
+    def player(self, state):
+        god, number = state
+        return god
+
+    def succ(self, state, action):
+        god, number = state
+        if action == '-':
+            return (-god, number-1)
+        elif action == '/':
+            return (-god, number//2)
+
+
+#general policy code
+
+def humanoid(game, state):
+    while True:
+        action = input('Operator action:')
+        if action in game.actions(state):
+            return action
+
+# Game controller
+
+policies = {+1: humanoid, -1: humanoid}
+game = HalvingGame(N=15)
+state = game.startState()
+
+while not game.isEnd(state):
+    print ('='*10, state)
+    player = game.player(state)
+    policy = policies [player]
+    action = policy(game, state)
+    state = game.succ(state, action)
+
+print('utility = {}'.format(game.utility(state)))
---
+++ AI_Stanford.U/Multiplying_Game.py
@@ -0,0 +1,56 @@
+class MultiplyingGame(object):
+    def __init__(self, N, player1, player2):
+        self.N = N
+        self.player1 = player1
+        self.player2 = player2
+
+    # state = (devil, number)
+
+    def startState(self):
+        return (+1, self.N)
+
+    def isEnd(self, state):
+        devil, number = state
+        return number == 0
+
+    def utility(self, state):
+        state = devil, number
+        assert number == 0
+        return devil * float('inf')
+
+    def actions(self, state):
+        return ['+', '*']
+
+    def player(self, state):
+        devil, number = state
+        return devil
+
+    def succ(self, state, action):
+        devil, number = state
+        if action == '+':
+            return (-devil, number-1)
+        elif action == '*':
+            return (-devil, number//2)
+
+#define policies player
+def humanoidPolicy(game, player1, player2):
+    while True:
+        action = input('Operator action:')
+        if action in game.actions(game.startState()):
+            return action
+
+# Game controller-
+policies = {+1: humanoidPolicy, -1: humanoidPolicy}
+player1 = 'Alice'
+player2 = 'Bob'
+game = MultiplyingGame(N=15, player1=player1, player2=player2)
+state = game.startState()
+
+while not game.isEnd(state):
+    print ('='*10, state)
+    player = game.player(state)
+    policy = policies [player]
+    action = policy(game, player1, player2)
+    state = game.succ(state, action)
+
+print('utility = {}'.format(game.utility(state)))
