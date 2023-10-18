f7b73985cf5ec9387e5761d666dd8cee20d39114
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-10-18 11:34:58 +0100

    Update Canada_Weather_Report.py

--- AI_Stanford.U/Canada_Weather_Report.py
+++ AI_Stanford.U/Canada_Weather_Report.py
@@ -28,8 +28,8 @@ windspeed = weather_data["wind"]["speed"]
 humidity = weather_data["main"]["humidity"]
 temperature = weather_data["main"]["temp"] * 9/5 + 32
 
-# This calculates the 'actual feel' temperature using the humidex formula
-actual_feel_temperature = 0.5*(temperature + 61.0 + (temperature - 61.0)*(0.944 + (0.0203*humidity) - (0.0001246*temperature*humidity)))
+#Data pulled from the API is in Kelvin so we have to convert this to Fahrenheit
+temperature_in_fahrenheit = (weather_data["main"]["temp"] - 273.15) * 9/5 + 32
 
 # Check if the air quality data has a "current" key
 if "current" in air_quality_data:
@@ -40,7 +40,7 @@ else:
     air_quality_index = None
 
 # Create the email message
-message = MIMEText(f"The weather in {CITY}, {COUNTRY} is {weather}. The windspeed is {windspeed} m/s. The humidity is {humidity}%. The temperature is {temperature} degrees Fahrenheit. The actual feel temperature is {actual_feel_temperature} degrees Fahrenheit. The air quality index is {air_quality_index}.")
+message = MIMEText(f"The weather in {CITY}, {COUNTRY} is {weather}. The windspeed is {windspeed} m/s. The humidity is {humidity}%. The temperature is {temperature_in_fahrenheit} degrees Fahrenheit. The air quality index is {air_quality_index}.")
 message["Subject"] = "Weather Report"
 message["From"] = aolaxxxxx
 message["To"] = aolaxxxxx
