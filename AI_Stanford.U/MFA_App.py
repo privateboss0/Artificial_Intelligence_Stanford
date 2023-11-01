e2fedd751ebd0cd5c3f27c00e3637e7b0b3469ca
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-01 20:01:30 +0100

    2FA with Google Authenticator & Recovery code app

--- AI_Stanford.U/MFA_App.py
+++ AI_Stanford.U/MFA_App.py
@@ -4,36 +4,31 @@ import qrcode
 
 secretkey = "PublicPrivate"
 
-# Counter variable to track the number of failed OTP attempts.
 counter = 0
 
-# Create a TOTP object.
-totp = pyotp.TOTP(secretkey)
+dotp = pyotp.TOTP(secretkey)
 
 while True:
   
-  otp_code = input("Enter the OTP from your Google Authenticator App: ")
+  dotp_code = input("Enter the OTP from your Google Authenticator App: ")
 
-  # Verify the OTP with secretkey.
-  if totp.verify(otp_code):
-    # Reset the counter variable.
+  if dotp.verify(dotp_code):
+
     counter = 0
 
-    # Authentication was successful.
+  
     print("Authentication successful!")
 
      # Make the API call.
     #Implement the API call here.
 
-
     break
   else:
-    # Increment the counter variable.
+   
     counter += 1
     
     print("Authentication failed, Please try again!")
 
-   
     if counter == 5:
       
       # Prompt the user to enter a recovery code after 5 failed OTP attempt by importing Recovery_Code_App
@@ -42,10 +37,9 @@ while True:
       recovery_code = input("Enter your recovery code: ")
 
       # Verify the recovery code.
-      hotp = pyotp.HOTP(secretkey)
-      if hotp.verify(recovery_code):
+      R_codes = pyotp.HOTP(secretkey)
+      if R_codes.verify(recovery_code):
         
-        # Reset the counter variable and authentication process.
         counter = 0
 
         print("Authentication successful!")
