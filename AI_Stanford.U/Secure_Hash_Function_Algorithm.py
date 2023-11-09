d29dc46d316482988b1f0be73e5dabeb53b7d49b
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-09 23:08:42 +0100

    SHA3-384, SHA3-512 and Blake2b

---
+++ AI_Stanford.U/Secure_Hash_Function_Algorithm.py
@@ -0,0 +1,41 @@
+""" This program takes an input and performs a one way hash function to ensure integrity using the 
+latest standard of the algorithm which are SHA3-384, SHA3-512 and Blake2b. """
+
+import hashlib
+
+def encodeMessage(msg):
+    encodedMsg = 0
+
+    for char in msg:
+        encodedMsg = encodedMsg << 8
+        encodedMsg = encodedMsg ^ ord(char)
+    return encodedMsg
+
+def sha3_384(data):
+
+  h = hashlib.sha3_384()
+  h.update(data)
+  return h.hexdigest()
+
+def sha3_512(data):
+
+  h = hashlib.sha3_512()
+  h.update(data)
+  return h.hexdigest()
+
+def blake2b(data):
+
+  h = hashlib.blake2b()
+  h.update(data)
+  return h.hexdigest()
+
+message = input('Type your message to be digested: ')
+
+encoded_message = message.encode()
+hash_digest06 = sha3_384(encoded_message)
+hash_digest07 = sha3_512(encoded_message)
+hash_digest09 = blake2b(encoded_message)
+
+print(f'The SHA3-384 hash of your message is:\n{hash_digest06}')
+print(f'\nThe SHA3-512 hash of your message is:\n{hash_digest07}')
+print(f'\nThe BLAKE2b hash of your message is:\n{hash_digest09}')
