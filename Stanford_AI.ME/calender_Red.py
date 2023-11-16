e6c5e0c7d44fb54b3b521f73dd76053efeffc931
privateboss1 <89046656+privateboss0@users.noreply.github.com>
2023-11-16 21:02:35 +0100

    calender_Red.py

--- Stanford_AI.ME/calender_Red.py
+++ Stanford_AI.ME/calender_Red.py
@@ -14,8 +14,8 @@ ddayd = r'\b{current_day}\b'.format(current_day=current_day)
 ddaydc = r"\033[31m\033[1m{current_day}\033[0m".format(current_day=current_day)
 
 # Replace the current year and date with Red colored current date
-calendar_text = calendar.month(current_year, current_month)
-calendar_text = re.sub(dday, ddayc, calendar_text)
-calendar_text = re.sub(ddayd, ddaydc, calendar_text)
+cal = calendar.month(current_year, current_month)
+cal = re.sub(dday, ddayc, cal)
+cal = re.sub(ddayd, ddaydc, cal)
 
-print(calendar_text)
+print(cal)
